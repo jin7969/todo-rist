0c2532970f7418ed5ce6b1c95e6f923004a3e5b1
Ahn <tn3748@naver.com>
2023-04-20 21:33:05 +0900

    feat: 다크모드 context 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList/TodoList";
 import Header from "./components/Header/Header";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 
@@ -9,10 +10,10 @@ function App() {
   const [filter, setFilter] = useState(filters[0]);
 
   return (
-    <>
+    <DarkModeProvider>
       <Header filters={filters} filter={filter} onChangeFilter={setFilter} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => useContext(DarkModeContext);
