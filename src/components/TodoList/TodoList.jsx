198cd1371ddcf89500443a763c240beed44039fa
Ahn <tn3748@naver.com>
2023-04-23 00:02:22 +0900

    feat: 투두 리스트 로컬 스토리지에 저장하고 불러오는 기능

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
@@ -9,20 +9,27 @@ const getFilteredTodo = (todos, filter) => {
   return todos.filter((todo) => todo.status === filter);
 };
 
+const getTodosFromLocalStorage = () => {
+  const todos = localStorage.getItem("todos");
+  return todos ? JSON.parse(todos) : [];
+};
+
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: "123", text: "장보기", status: "active" },
-    { id: "124", text: "공부하기", status: "active" },
-    { id: "125", text: "코딩하기", status: "completed" },
-  ]);
+  const [todos, setTodos] = useState(getTodosFromLocalStorage);
   const filteredTodos = getFilteredTodo(todos, filter);
 
-  const handleAddTodo = (value) => setTodos((prev) => [...prev, value]);
+  const handleAddTodo = (value) => {
+    setTodos((prev) => [...prev, value]);
+  };
 
   const handleUpdate = (value) =>
     setTodos(todos.map((todo) => (todo.id === value.id ? value : todo)));
 
   const handleDelete = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <section className={styles.container}>
